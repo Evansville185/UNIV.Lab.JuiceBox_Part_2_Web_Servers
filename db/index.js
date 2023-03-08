7faef5b5c2eeedc1756f3729d5e245f6e2d860d8
Evansville Rojas <evansville185@gmail.com>
2023-03-07 22:34:17 -0800

    added drop/create tables/test/rebuild functions

--- db/index.js
+++ db/index.js
@@ -3,18 +3,15 @@ const { Client } = require("pg"); // imports the pg module
 //The pg module is a Node.js package for working with PostgreSQL databases. The Client object is a class provided by the pg module that you can use to create a client connection to a PostgreSQL database.
 
 //supply the db name and location of the database
+//method 1 connection
 const client = new Client("postgres://postgres:1234@localhost:1234/juicebox-dev");
-
-// const client = new Client({
-//     connectionString: "postgres://1234@localhost:5432/juicebox-dev"
-//   });
   
-
+//method 2 connection
 // const client = new Client({
 //     user: 'postgres',
 //     password: '1234',
 //     host: 'localhost',
-//     port: 5432,
+//     port: 1234,
 //     database: 'juicebox-dev'
 //   });
   
@@ -23,18 +20,27 @@ module.exports = {
 };
 
 
-//-----------------------------------------------------------
-// async function getAllUsers() {
-//     const { rows } = await client.query(
-//       `SELECT id, username 
-//       FROM users;
-//     `);
+//5.1.4-----------------------------------------------------------
+async function getAllUsers() {
+	const { rows } = await client.query(
+	  `SELECT id, username 
+	  FROM users;
+	`);
   
-//     return rows;
-//   }
+	return rows;
+  }
   
-//   // and export them
-//   module.exports = {
-//     client,
-//     getAllUsers,
-//   }
+  // and export them
+  module.exports = {
+	client,
+	getAllUsers,
+  }
+
+
+// The Pattern
+// In general, in our db/index.js file we should provide the utility functions 
+// that the rest of our application will use. We will call them from the seed file, 
+// but also from our main application file.
+
+// That is where we are going to listen to the front-end code making AJAX 
+// requests to certain routes, and will need to make our own requests to our database.
--- db/seed.js
+++ db/seed.js
@@ -1,42 +1,142 @@
 // grab our client with destructuring from the export in index.js
-// grab our client with destructuring from the export in index.js
-const { client } = require('./index');
+//5.1.3------------------------------------------------
+// const { client } = require('./index');
 
-async function testDB() {
+// async function testDB() {
+//   try {
+//     // connect the client to the database, finally
+//     client.connect();
+
+//     // queries are promises, so we can await them
+//     const result = await client.query(`SELECT * FROM users;`);
+
+//     // for now, logging is a fine way to see what's up
+//     console.log(result);
+//   } catch (error) {
+//     console.error(error);
+//   } finally {
+//     // it's important to close out the client connection
+//     client.end();
+//   }
+// }
+
+// testDB();
+
+
+//5.1.3------------------------------------------------
+// async function testDB() {
+//   try {
+//     client.connect();
+
+//     const { rows } = await client.query(`SELECT * FROM users;`);
+//     console.log(rows);
+//   } catch (error) {
+//     console.error(error);
+//   } finally {
+//     client.end();
+//   }
+// }
+
+// testDB();
+
+//5.1.4-------------------------------------------------
+// const {
+//   client,
+//   getAllUsers
+// } = require('./index');
+
+// async function testDB() {
+//   try {
+//     client.connect();
+
+//     const users = await getAllUsers();
+//     console.log(users);
+//   } catch (error) {
+//     console.error(error);
+//   } finally {
+//     client.end();
+//   }
+// }
+
+// testDB();
+
+//5.1.5---------------------------------------------
+//Seeding
+// Making sure that the tables have correct definitions
+// Making sure that the tables have no unwanted data
+// Making sure that the tables have some data for us to play with
+// Making sure that the tables have necessary user-facing data
+
+
+const {
+  client,
+  getAllUsers
+} = require('./index');
+
+// this function should call a query which drops all tables from our database
+async function dropTables() {
   try {
-    // connect the client to the database, finally
-    client.connect();
+    console.log("Starting to drop tables...");
 
-    // queries are promises, so we can await them
-    const result = await client.query(`SELECT * FROM users;`);
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `); //instead of throwing an error, we see that we get a NOTICE instead! 
+    //It tries it, it fails, then it moves forward with whatever the rest of our query is
 
-    // for now, logging is a fine way to see what's up
-    console.log(result);
+    console.log("Finished dropping tables!");
   } catch (error) {
-    console.error(error);
-  } finally {
-    // it's important to close out the client connection
-    client.end();
+    console.error("Error dropping tables!");
+    throw error; // we pass the error up to the function that calls dropTables
   }
 }
 
-testDB();
+// this function should call a query which creates all tables for our database 
+async function createTables() {
+  try {
+    console.log("Starting to build tables...");
 
-//-------------------------------------------------
-//   const {
-//     client,
-//     getAllUsers // new
-//   } = require('./index');
-  
-//   async function testDB() {
-//     try {
-//       client.connect();
-  
-//       const users = await getAllUsers();
-//       console.log(users);
-//     } catch (error) {
-//       console.error(error);
-//     } finally {
-//       client.end();
-//     }
-//   }
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+      );
+    `);
+
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error; // we pass the error up to the function that calls createTables
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function testDB() {
+  try {
+    console.log("Starting to test database...");
+
+    const users = await getAllUsers();
+    console.log("getAllUsers:", users);
+
+    console.log("Finished database tests!");
+  } catch (error) {
+    console.error("Error testing database!");
+    throw error;
+  }
+}
+
+
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
