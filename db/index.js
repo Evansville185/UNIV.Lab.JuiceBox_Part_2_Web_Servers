c9bd87d8e33a9d277a6588258fbf96125b14af62
Evansville Rojas <evansville185@gmail.com>
2023-03-22 21:49:29 -0700

    TRYING TO LOAD TO RENDER!

--- db/index.js
+++ db/index.js
@@ -24,7 +24,7 @@ async function createUser({ username, password, name, location }) {
 			rows: [user],
 		} = await client.query(
 			`
-		INSERT INTO users(username, password, name, location) 
+		INSERT INTO users(username, password, name, location)
 		VALUES($1, $2, $3, $4) 
 		ON CONFLICT (username) DO NOTHING
 		RETURNING *;
